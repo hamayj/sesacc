ff203b9fd55f22ed3dac0ee29eaa0171ec3b73ee
hamayj <zmlzml0307@gmail.com>
2022-07-22 20:45:06 +0900

    study: local server, filezilla, server

---
+++ nodeJS/0722/apple.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.render("apple");
+});
+
+app.listen(port, () => {
+    console.log('Server port: ', port);
+})
+
+
+
--- nodeJS/0722/애벌레.js
+++ nodeJS/0722/애벌레.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8080;
 
 
 app.set('view engine', 'ejs');
@@ -8,17 +8,15 @@ app.set('view engine', 'ejs');
 //app.use( express.static('public'));
 // 정적 파일을 관리하는 것. 경로로 안쓸 것들을 어디에 있는지 미리 정의를 해놓는 것.
 // 퍼블릭을 정적파일을 놓는 곳으로 정의하는 것.
-app.use('/abc/aa', express.static('public'));
+app.use('/g', express.static('public'));
 // 인자를 두개로 만들어 줌.
 // /abc/aa로 가상 경로를 만들어준것 (abc가 public을 대신하는 것. )
 // 정적파일을 어떤 경로를 통해서 찾을 수 있는지 정의를 해둠.
 
 
 app.get('/', (req,res) => {
-    var list = ['apple', 'banana']; // 디비에서 정보를 가져왔다.
-    res.render("test", {A:list}); // 보낼때는 무조건 딕셔너리 형태로 보내자.
-}); // 배열은 보낼 수 없대. .. 키 값을 보내줘야된대..^^
-
+    res.render("애벌레");
+});
 //render 파일을 부르는거. ejs를 쓰기 때문에 ejs라는걸 이미 알고 있어.
 
 
