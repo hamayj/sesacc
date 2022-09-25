10b0bdb8dac3c2b2e9ed04913419c64db727c0fa
hamayj <zmlzml0307@gmail.com>
2022-09-25 22:04:07 +0900

    study: class컴포넌트 state예제3

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
 
-        this.state = {
-            number: 0,
-            fixedNumber: 0
-        };
-    }
+    //     this.state = {
+    //         number: 0,
+    //         fixedNumber: 0
+    //     };
+    // }
+    state = {
+        number: 0,
+        fixedNumber: 0
+    } 
+
     render(){
         const { number, fixedNumber } = this.state;
         return (
@@ -17,7 +22,17 @@ class Counter extends Component {
                 <h2>바뀌지 않는 값: {fixedNumber}</h2>
                 <button 
                     onClick={()=>{
-                    this.setState({ number : number + 1});
+                    this.setState(prevState => {
+                        return {
+                            number : prevState.number + 1
+                        }
+                    });
+
+                    // 위코드와 아래코드는 완전히 동일한 기능을 함.
+                    // 아래 코드는 함수에서 바로 객체를 반환함.
+                    this.setState(prevState => ({
+                        number: prevState.number + 1
+                    }));
                 }}>+1</button>
             </div>
         )
