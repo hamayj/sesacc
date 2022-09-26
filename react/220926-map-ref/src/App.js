c015990be997fc6ffd764fb5e10beee3f9462213
hamayj <zmlzml0307@gmail.com>
2022-09-26 14:24:05 +0900

    study: map, filter

--- react/220926-map-ref/src/App.js
+++ react/220926-map-ref/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+function App(){
+  const [list, setList] = useState(['test', 'testing', 'apple', 'sesac', 'animal']);
+  
+  let result = list.filter((value)=>{
+    return value.length > 4;
+  })
+
+  function filterCheck(){
+    let lists = ['test', 'testing', 'apple', 'sesac', 'animal'];
+    let newList = [];
+
+    for(let i=0; i<lists.length; i++){
+      if(lists[i].length > 4){
+        newList.push(list[i]);
+      }
+    }
+    let result = lists.filter((value)=>{
+      return value.length > 4;
+    })
+  }
+  function addElement(){
+    // react에서는 list.push를 사용하면 렌더링이 되지 않는다.
+    console.log([...list, 's']);
+    // 전개연산자
+    setList([...list, 's']);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {list.map((name, i) => {
+          return <li key={i}> {i}. {name} </li>
+        })}
+      </ul>
+      <ul>
+        {((name) => {
+          return name.includes("a");
+        })}
+        {result.map((name) => {
+          return <li>{name}</li>
+        })}
+      </ul>
+      <button onClick={addElement}>추가</button>
     </div>
   );
+  
 }
 
 export default App;
