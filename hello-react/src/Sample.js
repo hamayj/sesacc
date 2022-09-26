c33e2343fa6b278a3bbc75da20a0058807485cbc
hamayj <zmlzml0307@gmail.com>
2022-09-26 16:37:49 +0900

    study: 예제4. onDoubleClick과 filter를 사용한 요소 삭제

--- hello-react/src/Sample.js
+++ hello-react/src/Sample.js
@@ -36,7 +36,12 @@ const Sample = () => {
         setInputText('');
     }
 
-    const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+    const onRemove = id => {
+        const nextNames = names.filter(name => name.id !== id );
+        setNames(nextNames);
+    }
+
+    const namesList = names.map(name => <li key={name.id} onDoubleClick={() => onRemove(name.id)}>{name.text}</li>);
     return (
         <div>
             <input value={inputText} onChange={onChange}></input>
