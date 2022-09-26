2b13d0f22d8e09a633b35ae16fcdae8cf93a53be
hamayj <zmlzml0307@gmail.com>
2022-09-26 16:04:27 +0900

    study: react 예제3. 새로운 데이터를 등록할 수 있는 기능 구현

--- hello-react/src/Sample.js
+++ hello-react/src/Sample.js
@@ -23,11 +23,30 @@ const Sample = () => {
     const [nextId, setNextId] = useState(5);
     // 새로운 항목을 추가할 때 사용할 id
 
+    // 새로운 데이터를 등록할 수 있는 기능을 구현.
+    const onChange = (e) => setInputText(e.target.value);
+
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    }
+
     const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
     return (
-        <ul>
-            {namesList}
-        </ul>
+        <div>
+            <input value={inputText} onChange={onChange}></input>
+            <button onClick={onClick}>추가</button>
+
+            <ul>
+                {namesList}
+            </ul>       
+        </div>
+
     );
     // const fashion = ["청바지", "패딩", "반팔", "운동화"];
     // // 문자열로 구성된 배열 선언
