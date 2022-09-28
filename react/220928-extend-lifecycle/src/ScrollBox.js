5ee6b9793ac7b2bda9243d6cf3e114cb53b289d5
hamayj <zmlzml0307@gmail.com>
2022-09-28 21:12:22 +0900

    study: ref

---
+++ react/220928-extend-lifecycle/src/ScrollBox.js
@@ -0,0 +1,14 @@
+import React, {Fragment, Component} from 'react';
+
+class ScrollBox extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <button>버튼</button>
+                </div>
+            </div>
+
+        )
+    }
+} 
