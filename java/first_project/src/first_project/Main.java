7eb9f7bfe18cc95634deb36137ecbb3a5a8e548f
hamayj <zmlzml0307@gmail.com>
2022-11-07 15:17:16 +0900

    study: java print

---
+++ java/first_project/src/first_project/Main.java
@@ -0,0 +1,54 @@
+package first_project;
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//	
+//		System.out.println("선생님 안ㄹㄹ녕하세요.");
+//		boolean logic = true;
+//		char ch = 'A';
+//		int number = 2135;
+//		double number2 = 5.6;
+//		System.out.println(logic);
+//		System.out.println(ch);
+//		System.out.println(number);
+//		
+//		String str = "abcd";
+//		String str2 = new String("abcd");
+//		
+//		System.out.println("이름, 나이, 키, 결혼여부를 입력해주세요."); // 안내 문구
+//		// 나무늘보 3 24.4 false
+//		
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		String name = scanner.next(); // next 공백 이전의문자열을 읽어옴. 
+//		int age = scanner.nextInt();
+//		double height = scanner.nextDouble();
+//		boolean married = scanner.nextBoolean();
+//		
+//		System.out.println(name);
+//		System.out.println(age);
+//		System.out.println(height);
+//		System.out.println(married);
+//		
+//		scanner.close(); // 습관적으로 닫아주는 게 좋음.
+//		
+		double a = 5.2;
+		double b = 2.0;
+		System.out.println(a/b);
+		
+		int c = 5;
+		int d = 2; // 상단 a, b와 같은 값 넣어서 선언이 안됨.
+		System.out.println(c/d);
+		System.out.println((double) c/d); // 값을 실수로 변환시키겠다. (강제형 변환)
+		
+		
+		
+		System.out.println(5.0/2.0);
+		System.out.println(5/2);
+		
+	}
+
+}
