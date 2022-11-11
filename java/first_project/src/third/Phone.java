47192e40fe3d962953e335565c5f31551979c629
hamayj <zmlzml0307@gmail.com>
2022-11-11 16:08:13 +0900

    study: Java- interface

---
+++ java/first_project/src/third/Phone.java
@@ -0,0 +1,9 @@
+package third;
+
+public interface Phone {
+	public final String category = "phone";
+	public final String category2 = "phone";
+	
+	public abstract void sendCall();
+	public abstract void receiveCall();
+}
---
+++ java/first_project/src/third/Samsung.java
@@ -0,0 +1,15 @@
+package third;
+
+public class Samsung implements Phone {
+	@ Override
+	public void sendCall(){
+		System.out.println("뚜루루루");
+	};
+	@ Override
+	public void receiveCall() {
+		System.out.println("전화왔어요");
+	};
+	
+	
+	
+}
